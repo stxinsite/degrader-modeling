14d7015b15c3432e1e69216ed798d7ad2688106c
Daniel Park <daniel.park@roivant.com>
2021-09-10 11:10:24 -0400

    solve Douglass equilibrium system of equations

--- equilibria_modeling.py
+++ equilibria_modeling.py
@@ -1,75 +1,103 @@
 import numpy as np
 import pandas as pd
-import scipy
+from scipy.optimize import fsolve
 from math import sqrt
 from lmfit import minimize, Parameters
 
 """
 ABBREVIATIONS:
-- E: E3 ligase
-- D: degrader
-- T: target protein
-
-INDEPENDENT VARIABLES:
-- D
-
-DEPENDENT VARIABLES:
-- EDT
-
-PARAMETERS:
-- E_t
-- T_t
-- KAPPA
-- alpha
-- BETA
-
-CONSTANTS:
-- K_ED
-- K_DT
+- A: E3 ligase (target protein)
+- B: degrader
+- C: target protein (E3 ligase)
 """
 
 nanobret_df = pd.read_csv("data/2021-08-26_nano_bret_test.reformed.csv")
 nanobret_df['mBU'] = nanobret_df['Lum_610'] / nanobret_df['Lum_450'] * 1000
 nanobret_df.head()
 
-EDT = .00000000005
-alpha = 500
-E_t = 0.00000000005
-D_t = 0.00000796
-T_t = 0.00001
-K_ED = 0.0000018
-K_DT = 0.00000025
+"""
+GLOBAL CONSTANTS
+"""
+A_t = 5e-11
+C_t = 1e-5
+K_AB = 1.8e-6
+K_BC = 2.5e-7
+alpha = 10
 
-def ternary_quintic( D_in, EDT, E_t, T_t, K_ED, K_DT, alpha, beta, kappa):
-    D_t = kappa * D_in
+B_tmax = sqrt(K_BC) / (sqrt(K_AB) + sqrt(K_BC)) * (A_t + K_AB) + sqrt(K_AB) / (sqrt(K_AB) + sqrt(K_BC)) * (C_t + K_BC)
+TF_50 = A_t / 2 + K_AB / alpha
+TI_50 = C_t * (1 + alpha)
 
-    (
-    EDT**5 * (alpha - 1) * alpha**2 - \
-    EDT**4 * alpha * (alpha**2 * (2*E_t + D_t + 2*T_t) + \
-                      2*alpha * (K_ED + K_DT - E_t - T_t) - \
-                      2 * (K_ED + K_DT)) + \
-    EDT**3 * (alpha**3 * (E_t**2 + 2*E_t * (D_t + 2*T_t) + T_t * (2*D_t + T_t)) + \
-              alpha**2 * (-E_t * (D_t + 3*T_t - 2*K_ED - 3*K_DT) + \
-                          K_DT * (D_t + 2*T_t + K_ED) - \
-                          E_t**2 + D_t**2 - D_t*T_t - T_t**2 + \
-                          D_t*K_ED + 3*T_t*K_ED) + \
-              alpha * (K_DT**2 - 2*K_DT*(E_t + T_t + K_ED) + \
-                       K_ED*(K_ED - 2*(E_t + T_t))) - (K_ED - K_DT)**2) - \
-    EDT**2 * alpha *(alpha**2 * (D_t*T_t**2 + 2*E_t*T_t*(2*D_t+T_t) + E_t**2 *(D_t + 2*T_t)) - \
-                     (E_t*(D_t + T_t) + T_t*(T_t - D_t - K_ED))*K_ED - (E_t**2 + T_t*(D_t + K_ED) + E_t*(T_t - D_t + K_ED))*K_DT + \
-                     alpha * (-E_t**2 * (D_t + T_t - K_DT) + \
-                              E_t * (D_t**2 + D_t*(K_ED + K_DT - 2*T_t) - T_t**2 + 3*T_t*(K_ED + K_DT) + K_ED*K_DT) + \
-                              T_t * (D_t**2 + D_t*(K_ED + K_DT - T_t) + K_ED*(T_t + K_DT))) + \
-                     E_t * K_DT**2) + \
-    EDT * alpha**2 * E_t * T_t * (D_t**2 + \
-                                  D_t*(2*alpha*T_t + K_ED + K_DT - T_t) + \
-                                  E_t * ((2*alpha - 1)*D_t + alpha*T_t + K_DT) + \
-                                  K_ED*(T_t + K_DT)) - alpha**3 * E_t**2 * D_t * T_t**2
-    ) / alpha
+def equilibrium_sys(params, *constants):
+    A_t, C_t, K_AB, K_BC, alpha, B_t = constants
+    A = params[0]
+    C = params[1]
+    ABC = params[2]
 
-    return
+    F = np.empty((3))
+    F[0] = A + K_BC * ABC / (alpha * C) + ABC - A_t
+    F[1] = K_AB * K_BC * ABC / (alpha * A * C) + K_BC * ABC / (alpha * C) + K_AB * ABC / (alpha * A) + ABC - B_t
+    F[2] = C + K_AB * ABC / (alpha * A) + ABC - C_t
 
-def fit_ABC( data_obs, K_ED, K_DT, alpha = 1., beta = 0.5, kappa = 0.5):
+    return F
+
+init_params = np.array([A_t, C_t, 0.5*min(A_t, C_t)])  # initial guesses for roots
+sys_args = (A_t, C_t, K_AB, K_BC, alpha, B_tmax)
+
+roots = fsolve(equilibrium_sys, init_params, args=sys_args)  # solutions at B_tmax
+print(roots)
+np.isclose(equilibrium_sys(roots, *sys_args), [0.0, 0.0, 0.0])  # checks whether system is close to 0 at solutions
+
+sys_args = (A_t, C_t, K_AB, K_BC, alpha, TF_50)
+roots_1 = fsolve(equilibrium_sys, init_params, args=sys_args)
+print(roots_1)
+
+# [ABC] at TF_50 should be approximately 0.5 * [ABC]_max
+roots_1[2] / roots[2]
+
+sys_args = (A_t, C_t, K_AB, K_BC, alpha, TI_50)
+roots_2 = fsolve(equilibrium_sys, np.array([A_t * 1e-2, C_t * 1e-2, 0.5*min(A_t, C_t)]), args=sys_args)
+print(roots_2)
+np.isclose(equilibrium_sys(roots_2, *sys_args), [0.0, 0.0, 0.0])  # checks whether system is close to 0 at solutions
+
+# [ABC] at TI_50 should be approximately 0.5 * [ABC]_max
+roots_2[2] / roots[2]
+
+# [ABC] at TF_50 and TI_50 should be approximately equal
+np.isclose(roots_1[2], roots_2[2], rtol=1e-6)
+
+"""NON-COOPERATIVE EQUILIBRIA"""
+def noncoop_equilibrium(B_t):
+    A = A_t - (A_t + B_t + K_AB - sqrt((A_t + B_t + K_AB)**2 - 4*A_t*B_t)) / 2
+    C = C_t - (C_t + B_t + K_BC - sqrt((C_t + B_t + K_BC)**2 - 4*C_t*B_t)) / 2
+
+    phi_AB = A_t - A
+    phi_BC = C_t - C
+    ABC = phi_AB * phi_BC / B_t
+
+    return np.array([A, C, ABC])
+
+"""
+Check that non-coop(B_t) = coop(B_t | alpha = 1)
+"""
+noncoop_equilibrium(B_tmax)
+noncoop_equilibrium(B_t = 1.12e-6)
+noncoop_equilibrium(B_t = 2.45e-05)
+
+sys_args = (A_t, C_t, K_AB, K_BC, 1, B_tmax)
+roots_3 = fsolve(equilibrium_sys, init_params, args=sys_args)
+print(roots_3)
+
+sys_args = (A_t, C_t, K_AB, K_BC, 1, 1.12e-6)
+roots_4 = fsolve(equilibrium_sys, init_params, args=sys_args)
+print(roots_4)
+
+sys_args = (A_t, C_t, K_AB, K_BC, 1, 2.45e-05)
+roots_5 = fsolve(equilibrium_sys, init_params, args=sys_args)
+print(roots_5)
+
+
+def fit_equilibrium_sys( data_obs, K_ED, K_DT, alpha = 1., beta = 0.5, kappa = 0.5):
     """
     Fit equilibrium binding parameters to NanoBRET data.
 
@@ -127,21 +155,3 @@ def fit_ABC( data_obs, K_ED, K_DT, alpha = 1., beta = 0.5, kappa = 0.5):
 #
 # ## S38
 # ED * DT = (E * D**2 * T) / (K_ED * K_DT)
-#
-# ## S164
-# D_t = ((E_t + T_t - K_ED - K_DT +
-#     (alpha*(EDT - E_t)*(EDT - T_t) / EDT) +
-#     (E_t * (K_DT - K_ED) / (alpha*(EDT - E_t))) +
-#     (T_t * (K_ED - K_DT) / (alpha*(EDT - T_t))) +- # subtracted form represents left side of curve, summed form right side
-#     (alpha / EDT + 1 / (E_t - EDT) + 1 / (T_t - EDT)) *
-#     sqrt((alpha*(EDT - E_t)*(EDT - T_t) - EDT * K_ED)**2 -
-#          2*EDT*K_DT*(alpha*(EDT-E_t)*(EDT-T_t) + EDT*K_ED) +
-#          EDT**2 * K_DT**2
-#          )) / alpha
-# ) / 2
-
-# # NON-COOPERATIVE EQUILIBRIA
-# PHI_ED = (E_t + B_t + K_ED - sqrt((E_t + D_t + K_ED)**2 - 4 * E_t * D_t)) / 2
-# PHI_DT = (T_t + B_t + K_DT - sqrt((T_t + D_t + K_DT)**2 - 4 + T_t + D_t)) / 2
-#
-# EDT = PHI_ED * PHI_DT / D_t

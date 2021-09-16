88ed7c8cc5156ee7ba5e834f2ef267ee73db2d2a
Daniel Park <daniel.park@roivant.com>
2021-09-16 10:07:43 -0400

    save residual()

--- equilibria_modeling.py
+++ equilibria_modeling.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve, root
 from math import sqrt
-from lmfit import minimize, fit_report, Parameters
+from lmfit import Minimizer, minimize, fit_report, Parameters
 import matplotlib.pyplot as plt
 
 """
@@ -22,21 +22,9 @@ nanobret_df['Construct'].unique().tolist()
 
 nanobret_df['Time'].unique()
 
-"""
-GLOBAL CONSTANTS
-"""
-A_t = 5e-11
-C_t = 1e-5
-K_AB = 1.8e-6
-K_BC = 2.5e-7
-alpha = 10
-
-B_tmax = sqrt(K_BC) / (sqrt(K_AB) + sqrt(K_BC)) * (A_t + K_AB) + sqrt(K_AB) / (sqrt(K_AB) + sqrt(K_BC)) * (C_t + K_BC)
-TF_50 = A_t / 2 + K_AB / alpha
-TI_50 = C_t * (1 + alpha)
-
 """GENERAL SOLUTION (COOPERATIVE EQUILIBRIA)"""
 def equilibrium_sys(params, *constants):
+    """"System of equations describes concentrations at equilibrium"""
     A_t, C_t, K_AB, K_BC, alpha, B_t = constants
     A = params[0]
     C = params[1]
@@ -49,34 +37,33 @@ def equilibrium_sys(params, *constants):
 
     return F
 
-init_params = np.array([A_t, C_t, 0.5*min(A_t, C_t)])  # initial guesses for roots
-sys_args = (A_t, C_t, K_AB, K_BC, alpha, B_tmax)
+def f(params, B_t, *constants):
+    """"System of equations describes concentrations at equilibrium"""
+    A_t, C_t, K_AB, K_BC, alpha = constants
+    A = params[0]
+    C = params[1]
+    ABC = params[2]
 
-roots = fsolve(equilibrium_sys, init_params, args=sys_args)  # solutions at B_tmax
-print(roots)
-np.isclose(equilibrium_sys(roots, *sys_args), [0.0, 0.0, 0.0])  # checks whether system is close to 0 at solutions
+    F = np.empty((3))
+    F[0] = A + K_BC * ABC / (alpha * C) + ABC - A_t
+    F[1] = K_AB * K_BC * ABC / (alpha * A * C) + K_BC * ABC / (alpha * C) + K_AB * ABC / (alpha * A) + ABC - B_t
+    F[2] = C + K_AB * ABC / (alpha * A) + ABC - C_t
+    return F
 
-sys_args = (A_t, C_t, K_AB, K_BC, alpha, TF_50)
-roots_1 = fsolve(equilibrium_sys, init_params, args=sys_args)
-print(roots_1)
+def jac_equilibrium(params, *constants):
+    A_t, C_t, K_AB, K_BC, alpha, B_t = constants
+    A = params[0]
+    C = params[1]
+    ABC = params[2]
 
-# [ABC] at TF_50 should be approximately 0.5 * [ABC]_max
-roots_1[2] / roots[2]
+    return np.array([[1, -K_BC*ABC/(alpha*C**2), K_BC/(alpha*C) + 1],
+                     [-K_AB*K_BC*ABC/(alpha*A**2*C)-K_AB*ABC/(alpha*A**2),
+                      -K_AB*K_BC*ABC/(alpha*A*C**2)-K_BC*ABC/(alpha*C**2),
+                      K_AB*K_BC/(alpha*A*C)+K_BC/(alpha*C)+K_AB/(alpha*A) + 1],
+                     [-K_AB*ABC/(alpha*A**2), 1, K_AB/(alpha*A) + 1]])
 
-## for B_t > B_tmax, fsolve() is sensitive to the inital parameters
-sys_args = (A_t, C_t, K_AB, K_BC, alpha, TI_50)
-roots_2 = fsolve(equilibrium_sys, np.array([A_t * 1e-2, C_t * 1e-2, 0.5*min(A_t, C_t)]), args=sys_args)
-print(roots_2)
-np.isclose(equilibrium_sys(roots_2, *sys_args), [0.0, 0.0, 0.0])  # checks whether system is close to 0 at solutions
-
-# [ABC] at TI_50 should be approximately 0.5 * [ABC]_max
-roots_2[2] / roots[2]
-
-# [ABC] at TF_50 and TI_50 should be approximately equal
-np.isclose(roots_1[2], roots_2[2], rtol=1e-6)
-
-"""NON-COOPERATIVE EQUILIBRIA"""
 def noncoop_equilibrium(A_t, C_t, K_AB, K_BC, B_t):
+    """NON-COOPERATIVE EQUILIBRIUM"""
     A = A_t - (A_t + B_t + K_AB - sqrt((A_t + B_t + K_AB)**2 - 4*A_t*B_t)) / 2
     C = C_t - (C_t + B_t + K_BC - sqrt((C_t + B_t + K_BC)**2 - 4*C_t*B_t)) / 2
 
@@ -85,70 +72,6 @@ def noncoop_equilibrium(A_t, C_t, K_AB, K_BC, B_t):
     ABC = phi_AB * phi_BC / B_t
 
     return np.array([A, C, ABC])
-
-"""
-Check that non-coop(B_t) = coop(B_t | alpha = 1)
-"""
-noncoop_equilibrium(A_t, C_t, K_AB, K_BC, B_tmax)
-noncoop_equilibrium(A_t, C_t, K_AB, K_BC, B_t = 1.12e-6)
-noncoop_equilibrium(A_t, C_t, K_AB, K_BC, B_t = 2.45e-05)
-
-sys_args = (A_t, C_t, K_AB, K_BC, 1, B_tmax)
-roots_3 = fsolve(equilibrium_sys, init_params, args=sys_args)
-print(roots_3)
-
-sys_args = (A_t, C_t, K_AB, K_BC, 1, 1.12e-6)
-roots_4 = fsolve(equilibrium_sys, init_params, args=sys_args)
-print(roots_4)
-
-sys_args = (A_t, C_t, K_AB, K_BC, 1, 2.45e-05)
-roots_5 = fsolve(equilibrium_sys, init_params, args=sys_args)
-print(roots_5)
-
-"""FITTING PARAMETERS OF EQUILIBRIUM SYSTEM TO EMPIRICAL DATA"""
-"""DATA PROCESSING"""
-construct = 'VHL_WT SMARCA2_WT'
-construct_data = nanobret_df[nanobret_df['Construct'] == construct]
-min_time = construct_data['Time'].min()
-construct_data.loc[:, 'Minutes'] = (construct_data.loc[:,'Time'] - min_time) / pd.Timedelta(minutes=1)
-timepoints = []
-mean_corrected_mbus = []
-concentrations = []
-sds = []
-for timepoint in construct_data['Minutes'].unique().tolist():
-    construct_data_timepoint = construct_data[construct_data['Minutes'] == timepoint]
-    control_mean_mBU = construct_data_timepoint[~construct_data_timepoint['With_618']]['mBU'].mean()
-    control_sd_mBU = construct_data_timepoint[~construct_data_timepoint['With_618']]['mBU'].std()
-    construct_data_timepoint = construct_data_timepoint[construct_data_timepoint['With_618']]
-    for concentration in construct_data_timepoint['uM'].unique().tolist():
-        sample_mean_mBU = construct_data_timepoint[construct_data_timepoint['uM'] == concentration]['mBU'].mean()
-        sample_sd_mBU = construct_data_timepoint[construct_data_timepoint['uM'] == concentration]['mBU'].std()
-        sample_mean_corrected_mBU = sample_mean_mBU - control_mean_mBU
-        timepoints.append(timepoint)
-        mean_corrected_mbus.append(sample_mean_corrected_mBU)
-        concentrations.append(concentration)
-        sds.append(sample_sd_mBU)
-
-construct_df = pd.DataFrame({'Minutes': timepoints,
-                             'uM': concentrations,
-                             'M': conc_molar,
-                             'mBU_corrected': mean_corrected_mbus,
-                             'stdev': sds,
-                             'Construct': construct})
-construct_df = construct_df.sort_values(by=['Minutes','uM'])
-construct_df.head()
-construct_df.shape
-
-construct_df['Minutes'].unique().tolist()
-
-data_obs = construct_df[construct_df['Minutes'] == 96.05][['uM', 'mBU_corrected']].to_numpy()
-data_obs
-
-# SPR measurements of ACBI1 PROTAC K_d's from http://cadd.zju.edu.cn/protacdb/compound/dataset=protac&id=798
-# convert to micromolar
-# original units in nanomolar
-K_AB = 250e-3
-K_BC = 1800e-3
 
 def residual(params, data_obs, K_AB, K_BC):
     A_t = params['A_t']
@@ -160,16 +83,43 @@ def residual(params, data_obs, K_AB, K_BC):
     B_t = np.multiply(data_obs[:,0], kappa)  # B_t = kappa * B_x
 
     ABC_pred = []  # predicted [ABC] that satisfies equilibrium system
+    init_params = np.array([A_t*0.3, C_t*0.3, 1])  # initial guesses for [A], [C], [ABC]
     for B_i in B_t:
-        init_params = np.array([0.5*A_t, 0.5*C_t, 0.5*min(A_t, B_i, C_t)])  # initial guesses for [A], [C], [ABC]
         sys_args = (A_t, C_t, K_AB, K_BC, alpha, B_i)
-        roots = fsolve(equilibrium_sys, init_params, args=sys_args)
+        roots = root(equilibrium_sys, init_params, jac=jac_equilibrium, args=sys_args)
         # print(roots)
         # print(np.isclose(equilibrium_sys(roots, *sys_args), [0.0, 0.0, 0.0]).all())
-        ABC_pred.append(roots[2])
+        ABC_pred.append(roots.x[2])
 
     y_pred = np.multiply(np.array(ABC_pred), beta)  # ^mBU = ^[ABC] * beta
-    return data_obs[:,1] - y_pred
+    resid = data_obs[:,1] - y_pred
+    squared_resid = np.square(resid)
+    # print(squared_resid.mean())
+    return resid
+
+def df_dx(params, data_obs, K_AB, K_BC, y):
+    A_t = params['A_t']
+    C_t = params['C_t']
+    alpha = params['alpha']
+    beta = params['beta']
+    kappa = params['kappa']
+
+    return np.array([[1, 0, K_BC*ABC/(alpha**2*C), 0],
+                     [0, 0, K_AB*K_BC*ABC/(alpha**2*A*C)+K_AB*ABC/(alpha**2*A)+K_BC*ABC/(alpha**2*C), B_x],
+                     -, 1, K_AB*ABC/(alpha**2*A), 0])
+
+solve_ABC()
+
+# def residual(params, data, K_AB, K_BC):
+#         A_t = params['A_t']
+#         C_t = params['C_t']
+#         alpha = params['alpha']
+#         beta = params['beta']
+#         kappa = params['kappa']
+#         B_t = kappa * data[:,0]
+#
+#         solve_ABC(A_t, C_t, K_AB, K_BC, alpha, B_t)
+#         return ABC_pred - data[:,1]
 
 def fit_equilibrium_sys(data_obs, K_AB, K_BC, A_t=1., C_t=1., alpha=1., beta=1., kappa=0.5):
     """
@@ -196,38 +146,70 @@ def fit_equilibrium_sys(data_obs, K_AB, K_BC, A_t=1., C_t=1., alpha=1., beta=1.,
 
     params.add( 'A_t', value = A_t, min = 0.)
     params.add( 'C_t', value = C_t, min = 0.)
-    params.add( 'alpha', value = alpha, min = 1.)
+    params.add( 'alpha', value = alpha, min = 0.)
     params.add( 'beta', value = beta, min = 0.)
     params.add( 'kappa', value = kappa, min = 0., max = 1.)
 
+    # def mse(params, iter, resid, *fcn_args, **fcn_kws):
+    #     squared_resid = np.square(resid)
+    #     return squared_resid.mean()
+
     result = minimize( residual, params, args = (data_obs, K_AB, K_BC),
-                       method = 'least_squares', nan_policy = 'omit')
+                       method = 'leastsq', nan_policy = 'omit')
     return result
 
-res_all = []
-for minute in construct_df['Minutes'].unique().tolist():
-    data_obs = construct_df[construct_df['Minutes'] == minute][['uM', 'mBU_corrected']].to_numpy()
+"""
+UNIT TESTING
+units in micromolar
+"""
+A_t = 1
+B_x = np.array([0.0, 0.05, 0.1, 0.5, 1, 1.5, 3, 5])
+C_t = 2
+alpha = 1.5
+kappa = 0.75
+K_AB = 250e-3
+K_BC = 1800e-3
+beta = 5
+B_t = np.multiply(B_x, kappa)
+init_params = np.array([0.5*A_t, 0.5*C_t, 1])  # initial guesses for [A], [C], [ABC]
+ABC = []  # predicted [ABC] that satisfies equilibrium system
+for B_i in B_t:
+    sys_args = (A_t, C_t, K_AB, K_BC, alpha, B_i)
+    roots = fsolve(equilibrium_sys, init_params, args=sys_args)
+    print(roots)
+    print(np.isclose(equilibrium_sys(roots, *sys_args), [0.0, 0.0, 0.0]).all())
+    ABC.append(roots[2])
 
-    res = fit_equilibrium_sys(data_obs, K_AB, K_BC, A_t = 5, C_t = 5, alpha = 1, beta = 1, kappa = 1)
-    res_all.append(res)
+mBU = np.multiply(np.array(ABC), beta)  # mBU = [ABC] * beta
+plt.plot(B_x, mBU, label = 'simulated', color = 'cyan')
+plt.xscale('log')
+plt.legend()
+plt.title('Simulated mBU data')
+plt.xlabel('B_t')
+plt.show()
 
-len(res_all)
-minutes_list = construct_df['Minutes'].unique().tolist()
-for i in range(len(res_all)):
-    print(f"Minute: {minutes_list[i]}")
-    res = res_all[i]
-    res.params.pretty_print()
+true_data = np.stack((B_x, mBU), axis=1)
+# fit with init vals = true vals
+fit_equilibrium_sys(data_obs = true_data, K_AB=K_AB, K_BC=K_BC, A_t=A_t, C_t=C_t, alpha=alpha, beta=beta, kappa=kappa)
 
-    mBU_pred = data_obs[:,1] - res.residual
 
-    plt.plot(data_obs[:,0], data_obs[:,1], label = 'observed', color = 'cyan')
-    plt.plot(data_obs[:,0], mBU_pred, label = 'predicted', color = 'coral')
-    plt.xscale('log')
-    plt.legend()
-    plt.show()
+# good fit
+fit_equilibrium_sys(data_obs = true_data, K_AB=K_AB, K_BC=K_BC, A_t=5, C_t=5, alpha=3, beta=5, kappa=0.5)
 
-    # print(fit_report(res))
+fit_equilibrium_sys(data_obs=true_data, K_AB=K_AB, K_BC=K_BC, A_t=7, C_t=10, alpha=2, beta=5, kappa=0.5)
 
+fit_equilibrium_sys(data_obs=true_data, K_AB=K_AB, K_BC=K_BC, A_t=10, C_t=20, alpha=2, beta=10, kappa=0.5)
+
+# init vals for A_t, C_t greater than true vals by factor of 5 is unstable
+res_4 = fit_equilibrium_sys(data_obs=true_data, K_AB=K_AB, K_BC=K_BC, A_t=10, C_t=10, alpha=2, beta=5, kappa=0.5)
+v = res_4.params
+res_5 = fit_equilibrium_sys(data_obs=true_data, K_AB=K_AB, K_BC=K_BC,
+                            A_t = v['A_t'].value, C_t = v['C_t'].value, alpha=v['alpha'].value,
+                            beta=v['beta'].value, kappa=v['kappa'].value)
+res_5
+# very poor fit
+res_2 = fit_equilibrium_sys(data_obs = true_data, K_AB=K_AB, K_BC=K_BC, A_t=A_t*10, C_t=C_t*10, alpha=1, beta=1, kappa=0.5)
+res_2
 
 # ## S18
 # E = K_ED * (E_t - EDT) / (K_ED + D)

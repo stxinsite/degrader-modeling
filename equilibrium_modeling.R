fa25937fe158fc8d4e3e50be95fbf489d1a644ac
Daniel Park <daniel.park@roivant.com>
2021-09-29 15:58:08 -0400

    exponential transformation in equilibrium system

--- equilibrium_modeling.R
+++ equilibrium_modeling.R
@@ -1,34 +1,56 @@
-library(rootSolve)
+library(nleqslv)
 library(nimble)
+library(basicMCMCplots)
+library(coda)
+library(dplyr)
+library(ggplot2)
 
-equilibriumSys <- function(x, parms){
-  A_t = parms[1]
-  C_t = parms[2]
-  K_AB = parms[3]
-  K_BC = parms[4]
-  alpha = parms[5]
-  B_t = parms[6]
+equilibrium_f <- function(x, parms) {
+  A_t <- parms[1]
+  B_t <- parms[2]
+  C_t <- parms[3]
+  K_AB <- parms[4]
+  K_BC <- parms[5]
+  alpha <- parms[6]
 
-  A = x[1]
-  C = x[2]
-  ABC = x[3]
+  A <- exp(x[1])
+  C <- exp(x[2])
+  ABC <- exp(x[3])
 
-  F1 = A + K_BC * ABC / (alpha * C) + ABC - A_t
-  F2 = K_AB * K_BC * ABC / (alpha * A * C) + K_BC * ABC / (alpha * C) + K_AB * ABC / (alpha * A) + ABC - B_t
-  F3 = C + K_AB * ABC / (alpha * A) + ABC - C_t
-
+  F1 <- A + K_BC * ABC / (alpha * C) + ABC - A_t
+  F2 <- K_AB * K_BC * ABC / (alpha * A * C) + K_BC * ABC / (alpha * C) + K_AB * ABC / (alpha * A) + ABC - B_t
+  F3 <- C + K_AB * ABC / (alpha * A) + ABC - C_t
   return(c(F1 = F1, F2 = F2, F3 = F3))
 }
 
-noncoopEquilibrium <- function(parms){
-  A_t = parms[1]
-  C_t = parms[2]
-  K_AB = parms[3]
-  K_BC = parms[4]
-  B_t = parms[5]
+equilibrium_jac <- function(x, parms) {
+  K_AB <- parms[4]
+  K_BC <- parms[5]
+  alpha <- parms[6]
 
-  A <- A_t - (A_t + B_t + K_AB - sqrt((A_t + B_t + K_AB)**2 - 4*A_t*B_t)) / 2
-  C <- C_t - (C_t + B_t + K_BC - sqrt((C_t + B_t + K_BC)**2 - 4*C_t*B_t)) / 2
+  A <- exp(x[1])
+  C <- exp(x[2])
+  ABC <- exp(x[3])
+
+  df_dy <- rbind(
+    c(A, -K_BC * ABC / (alpha * C), K_BC * ABC / (alpha * C) + ABC),
+    c(-K_AB * K_BC * ABC / (alpha * A * C) - K_AB * ABC / (alpha * A),
+      -K_AB * K_BC * ABC / (alpha * A * C) - K_BC * ABC / (alpha * C),
+      K_AB * K_BC * ABC / (alpha * A * C) + K_BC * ABC / (alpha * C) + K_AB * ABC / (alpha * A) + ABC),
+    c(-K_AB * ABC / (alpha * A), C, K_AB * ABC / (alpha * A) + ABC)
+  )
+  return(df_dy)
+}
+
+noncoop_f <- function(parms) {
+  A_t <- parms[1]
+  B_t <- parms[2]
+  C_t <- parms[3]
+  K_AB <- parms[4]
+  K_BC <- parms[5]
+
+  A <- A_t - (A_t + B_t + K_AB - sqrt((A_t + B_t + K_AB)**2 - 4 * A_t * B_t)) / 2
+  C <- C_t - (C_t + B_t + K_BC - sqrt((C_t + B_t + K_BC)**2 - 4 * C_t * B_t)) / 2
 
   phi_AB <- A_t - A
   phi_BC <- C_t - C
@@ -36,122 +58,207 @@ noncoopEquilibrium <- function(parms){
   return(c(A, C, ABC))
 }
 
-parms <- c(5e-11, 1e-5, 1.8e-6, 2.5e-7, 1, 1.12e-6)
-init <- noncoopEquilibrium(c(5e-11, 1e-5, 1.8e-6, 2.5e-7, 1.12e-6))
-init
+################################################################################
+## TESTING ROOT-FINDING ##
+################################################################################
+test_roots <- function(parms) {
+  noncoop_sols <- noncoop_f(head(parms, -1))
+  init_guess <- log(noncoop_sols)
+  equilibrium_roots <- nleqslv(
+    x = init_guess,
+    fn = equilibrium_f,
+    # jac = equilibrium_jac,
+    parms = parms
+  )
+  coop_sols <- exp(equilibrium_roots$x)
+  print(noncoop_sols)
+  print(coop_sols)
+}
 
-equilibriumRoots <- multiroot(equilibriumSys, init, parms = parms)
-equilibriumRoots$root
+test_roots(c(5e-11, 7.5e-2, 1e-5, 5e-8, 1e-9, 1))
 
+test_roots(c(5e-11, 1.2e-2, 1e-5, 5e-8, 1e-9, 1))
+
+test_roots(c(5e-11, 9.18e-9, 1e-5, 5e-8, 1e-9, 1))
+
+test_roots(c(5e-11, 6.24e-15, 1e-5, 5e-8, 1e-9, 1))
+
+test_roots(c(5e-11, 1.12e-6, 1e-5, 1.8e-6, 2.5e-7, 1))
+
+test_roots(c(5e-11, 1.12e-6, 1e-5, 1.8e-6, 2.5e-7, 30))
+################################################################################
+## NIMBLE FUNCTIONS ##
+################################################################################
+wrap_solve <- function(A_t, B_t, C_t, K_AB, K_BC, alpha) {
+  parms <- c(A_t, B_t, C_t, K_AB, K_BC, alpha)
+  init_guess <- noncoop_f(head(parms, -1))
+  equilibrium_roots <- nleqslv(
+    x = init_guess,
+    fn = equilibrium_f,
+    jac = equilibrium_jac,
+    parms = parms
+  )
+  return(equilibrium_roots$x[3])
+}
+
+nimble_solve <- nimbleRcall(
+  prototype = function(A_t = double(0),
+                       B_t = double(0),
+                       C_t = double(0),
+                       K_AB = double(0),
+                       K_BC = double(0),
+                       alpha = double(0)) {},
+  returnType = double(0),
+  Rfun = "wrap_solve"
+)
+
+################################################################################
+## NANOBRET DATA ##
+################################################################################
+nanobret_csv <- read.csv("data/corrected_nanobret_df.csv")
+
+sorted_min <- sort(unique(nanobret_csv$Minutes))
+sorted_min[6:7]
+
+nanobret_96 <- nanobret_csv %>% filter(Minutes %in% sorted_min[6:7])
+nanobret_96$mBU_corrected <- nanobret_96$mBU_corrected - min(nanobret_96$mBU_corrected)
+
+ggplot(nanobret_96, aes(x = uM, y = mBU_corrected, color = Construct)) +
+  geom_point() +
+  scale_x_log10()
+
+nanobret_96 <- nanobret_96 %>% mutate(construct_int = recode(Construct,
+  "VHL_WT SMARCA2_L1415S" = 1,
+  "VHL_WT SMARCA2_E1420S" = 2,
+  "VHL_R69Q SMARCA2_WT" = 3,
+  "VHL_Y112F SMARCA2_WT" = 4,
+  "VHL_WT SMARCA2_WT" = 5
+))
+
+head(nanobret_96)
+################################################################################
+## NANOBRET NIMBLE MODEL ##
+################################################################################
+# nimble code
 code <- nimbleCode({
-  multiroot(equilibriumSys, noncoopEquilibrium(c(5e-11, 1e-5, 1.8e-6, 2.5e-7, 1.12e-6)), c(5e-11, 1e-5, 1.8e-6, 2.5e-7, 1, 1.12e-6))
-    for (i in 1:G) {
-        a[i] ~ dgamma(1, .001)
-        b[i] ~ dgamma(1, .001)
-        for (j in 1:N) {
-            r[i,j] ~ dbin(p[i,j], n[i,j])
-            p[i,j] ~ dbeta(a[i], b[i])
-        }
-        mu[i] <- a[i] / (a[i] + b[i])
-        theta[i] <- 1 / (a[i] + b[i])
-    }
+  A_t ~ dgamma(.001, .001)
+  C_t ~ dgamma(.001, .001)
+  relative_constraint ~ dconstraint(A_t < C_t)
+  # ratio_constraint ~ dconstraint(0.1 <= (A_t / C_t))
+  beta ~ dgamma(.001, .001)
+  kappa ~ dunif(0, 1)
+  for (i in 1:n_constructs) {
+    alpha[i] ~ dgamma(mean = 30, sd = 3)
+  }
+
+  for (i in 1:N) {
+    B_t[i] <- B_x[i] * kappa
+    ABC[i] <- nimble_solve(A_t, B_t[i], C_t, K_AB, K_BC, alpha[construct_int[i]])
+    mu[i] <- ABC[i] * beta
+    y[i] ~ dnorm(mean = mu[i], sd = 0.05)
+  }
 })
 
 # list of fixed constants
-constants <- list(G = 2,
-                  N = 16,
-                  n = matrix(c(13, 12, 12, 11, 9, 10, 9,  9, 8, 11, 8, 10, 13,
-                      10, 12, 9, 10,  9, 10, 5,  9,  9, 13, 7, 5, 10, 7,  6,
-                      10, 10, 10, 7), nrow = 2))
+constants <- list(
+  n_constructs = length(unique(nanobret_96$construct_int)),
+  B_x = nanobret_96$uM,
+  construct_int = nanobret_96$construct_int,
+  N = nrow(nanobret_96),
+  K_AB = 1.8e-6,
+  K_BC = 2.5e-7
+)
 
 # list specifying model data
-data <- list(r = matrix(c(13, 12, 12, 11, 9, 10,  9, 9, 8, 10, 8, 9, 12, 9,
-                 11, 8, 9,  8,  9,  4, 8,  7, 11, 4, 4, 5 , 5, 3,  7, 3,
-                 7, 0), nrow = 2))
+data <- list(
+  y = nanobret_96$mBU_corrected,
+  relative_constraint = 1
+  # ratio_constraint = 1
+)
 
 # list specifying initial values
-inits <- list(a = c(1, 1),
-              b = c(1, 1),
-              p = matrix(0.5, nrow = 2, ncol = 16),
-              mu    = c(.5, .5),
-              theta = c(.5, .5))
+inits <- list(
+  A_t = 1,
+  C_t = 2,
+  beta = 1,
+  kappa = 0.75,
+  alpha = rep(30, length(unique(nanobret_96$construct_int)))
+)
 
 # build the R model object
-Rmodel <- nimbleModel(code = code,
-                      constants = constants,
-                      data      = data,
-                      inits     = inits)
+Rmodel <- nimbleModel(
+  code = code,
+  constants = constants,
+  data = data,
+  inits = inits
+)
+Rmodel$calculate()
 
-###########################################
-##### MCMC configuration and building #####
-###########################################
+################################################################################
+### MCMC configuration and building ##
+################################################################################
+conf <- configureMCMC(Rmodel)
 
-# generate the default MCMC configuration;
-# only wish to monitor the derived quantity "mu"
-mcmcConf <- configureMCMC(Rmodel, monitors = "mu")
+Rmcmc <- buildMCMC(conf)
 
-# check the samplers assigned by default MCMC configuration
-mcmcConf$printSamplers()
-
-# double-check our monitors, and thinning interval
-mcmcConf$printMonitors()
-
-# build the executable R MCMC function
-mcmc <- buildMCMC(mcmcConf)
-
-# let's try another MCMC, as well,
-# this time using the crossLevel sampler for top-level nodes
-
-# generate an empty MCMC configuration
-# we need a new copy of the model to avoid compilation errors
-Rmodel2 <- Rmodel$newModel()
-mcmcConf_CL <- configureMCMC(Rmodel2, nodes = NULL, monitors = "mu")
-
-# add two crossLevel samplers
-mcmcConf_CL$addSampler(target = c("a[1]", "b[1]"), type = "crossLevel")
-mcmcConf_CL$addSampler(target = c("a[2]", "b[2]"), type = "crossLevel")
-
-# let's check the samplers
-mcmcConf_CL$printSamplers()
-
-# build this second executable R MCMC function
-mcmc_CL <- buildMCMC(mcmcConf_CL)
-
-
-###################################
-##### compile to C++, and run #####
-###################################
-
-# compile the two copies of the model
+################################################################################
+### compile to C++, and run ###
+################################################################################
 Cmodel <- compileNimble(Rmodel)
+Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
 
-Cmodel2 <- compileNimble(Rmodel2)
+################################################################################
+## run multiple MCMC chains ###
+################################################################################
+samples <- runMCMC(Cmcmc, niter = 10000, nburnin = 1000, samplesAsCodaMCMC = TRUE)
+samplesSummary(samples)
 
-# compile both MCMC algorithms, in the same
-# project as the R model object
-# NOTE: at this time, we recommend compiling ALL
-# executable MCMC functions together
-Cmcmc <- compileNimble(mcmc, project = Rmodel)
-Cmcmc_CL <- compileNimble(mcmc_CL, project = Rmodel2)
+samplesPlot(samples)
 
-# run the default MCMC function,
-# and example the mean of mu[1]
-Cmcmc$run(1000)
-cSamplesMatrix <- as.matrix(Cmcmc$mvSamples) # alternative: as.list
-mean(cSamplesMatrix[, "mu[1]"])
+effectiveSize(samples)
 
-# run the crossLevel MCMC function,
-# and examine the mean of mu[1]
-Cmcmc_CL$run(1000)
-cSamplesMatrix_CL <- as.matrix(Cmcmc_CL$mvSamples)
-mean(cSamplesMatrix_CL[, "mu[1]"])
+nanobret_96_1 <- nanobret_96 %>% filter(construct_int == 1)
+for (B_t in nanobret_96_1$uM) {
+  parms <- c(.697, B_t, 2.594, 1.8e-6, 2.5e-7, 27.327)
+  noncoop_sols <- noncoop_f(head(parms, -1))
+  init_guess <- log(noncoop_sols + 1)
+  equilibrium_roots <- nleqslv(
+    x = init_guess,
+    fn = equilibrium_f,
+    # jac = equilibrium_jac,
+    parms = parms
+  )
+  print(exp(equilibrium_roots$x))
+  # ABC <- wrap_solve(A_t = 0.697, B_t = B_t, C_t = 2.594, K_AB = 1.8e-6, K_BC = 2.5e-7, alpha = 27.327)
+  # pred <- ABC * 7.437
+  # print(ABC)
+  # print(pred)
+}
 
+parms <- c(.697, 5.0, 2.594, 1.8e-6, 2.5e-7, 27.327)
+init_guess <- noncoop_f(head(parms, -1))
+init_guess
+log(init_guess)
+equilibrium_roots <- nleqslv(
+  x = log(init_guess),
+  fn = equilibrium_f,
+  # jac = equilibrium_jac,
+  parms = parms,
+  jacobian = TRUE
+)
 
-###################################
-#### run multiple MCMC chains #####
-###################################
+equilibrium_jac(x = c(2.916e-7, 2.695e-7, .362), parms = parms)
+
+equilibrium_roots$fvec
+exp(equilibrium_roots$x)
+
+wrap_solve(A_t = 0.697, B_t = 5, C_t = 2.594, K_AB = 1.8e-6, K_BC = 2.5e-7, alpha = 27.327)
+
+ggplot(nanobret_96_1, aes(x = uM, y = mBU_corrected)) +
+  geom_point() +
+  scale_x_log10()
 
 # run 3 chains of the crossLevel MCMC
-samplesList <- runMCMC(Cmcmc_CL, niter=1000, nchains=3)
+samplesList <- runMCMC(Cmcmc, niter = 1000, nchains = 3)
 
 lapply(samplesList, dim)
